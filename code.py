de32e0f29e42b0d790170d9d30a8a8a021bb0e5e
Toer <toervanholstein@gmail.com>
2021-01-06 13:12:40 +0100

    lezertje voor wijk 1

--- code.py
+++ code.py
@@ -14,3 +14,15 @@ with open('Huizen&Batterijen/district_1/district-1_batteries.csv') as csv_file:
             print(f'Coordinates are: {row[0]}, Capaciteit is: {row[1]}.')
             line_count += 1
         print(f'Processed {line_count} lines.')
+
+with open('Huizen&Batterijen/district_1/district-1_houses.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'Coordinates are: {row[0]}, Output is: {row[1]}.')
+            line_count += 1
+        print(f'Processed {line_count} lines.')
