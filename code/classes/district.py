e32ad62acd5d21152f374f9ce08d96ce023d11a0
Toer <toervanholstein@gmail.com>
2021-01-07 16:35:09 +0100

    commented out text in district.py untill json function is implemented

--- code/classes/district.py
+++ code/classes/district.py
@@ -10,13 +10,23 @@ class District:
     #def __repr__(self):
         #return json.dumps({"district":self.id, "costs-shared": self.costs_shared})
 
-    def json_output(self):
-        data = {}
+    # def json_output(self, object):
+    #     data = json.dumps(object)
+    #     print(data)
 
-        for battery in self.batteries:
-            data['capacity'] = battery.capacity
-            data['location'] = battery.x_coordinate,battery.y_coordinate
-            data['houses'] = []
-            for house in houses:
-                if house.connected_battery == battery.id:
-                    data['houses'].append({'location': house.x_coordinate,house.y_coordinate, 'output': house.output, 'cables': house.cables})
+    # def toJSON(self):
+    #     return json.dumps(self, default=lambda o: o.__dict__,
+    #                       sort_keys=True, indent=4)
+
+
+    #     with open('data.txt', 'w') as outfile:
+    #         json.dump(data, outfile)
+        # print(data)
+        # for battery in self.batteries:
+        #     data['capacity'] = battery.capacity
+        #     data['location'] = battery.x_coordinate,battery.y_coordinate
+        #     data['houses'] = []
+        #     for house in houses:
+        #         if house.connected_battery == battery.id:
+        #             #data['houses'].append({'location': house.x_coordinate,house.y_coordinate, 'output': house.output, 'cables': house.cables})
+        #             pass
