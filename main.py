6946356e27df28a6a9a99232fa2d55a04e133011
Neeltjepegu <NeeltjePegu>
2021-01-12 23:37:06 +0100

    added a script that will run randomizer for i times

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from code.algorithms.closest import closest_assignment
 from code.classes.cable import Cable
 from code.functions.visualise import visualise
 from pprint import pprint
-import matplotlib.pyplot as plt
-
+import matplotlib.pyplot as plot
+from multiple_average import run_multiple_random, run_multiple_closest
 
 if __name__ == '__main__':
     list_battery_objects = load_battery_file('data/Huizen&Batterijen/district_1/district-1_batteries.csv')
@@ -26,3 +26,24 @@ if __name__ == '__main__':
     # a = visualise(randomized_district)
     # print(f"Cost shared: {randomized_district.costs_shared}")
 
+
+    # average calculator
+    # cost_sum = 0
+    # i = 0
+    # list_costs = []
+    # while i < 100:
+    #     r = District(id, list_house_objects, list_battery_objects)
+    #     randomized_district = random_assignment(r)
+    #     cost_sum+=randomized_district.costs_shared
+    #     i+=1
+    #     list_costs.append(randomized_district.costs_shared)
+    #     print(f"Cost shared: {randomized_district.costs_shared}")
+
+    # print(list_costs)
+    # cost_average = cost_sum/100
+    # print(cost_sum)
+    # print(cost_average)
+
+    # multiple_random = run_multiple_random(d)
+
+    # multiple_closest = run_multiple_closest(d)
