aed85a2860f38bc9e63ffae4d8d2cf4a8ba1f717
Toer <toervanholstein@gmail.com>
2021-01-15 17:00:05 +0100

    fixed little bugs in kmeans. still starts the same way every time, needs fixing.

--- code/algorithms/kmeans.py
+++ code/algorithms/kmeans.py
@@ -3,35 +3,47 @@ from code.functions.lay_cables import create_cable
 from code.functions.find_closest_cable import find_closest_cable
 from code.functions.random_battery import random_battery
 from code.classes.exceptions import NoBatteryError
+from code.functions.visualise import visualise
+import time
 import copy
 
 def k_means(district):
     random_coordinates = random_battery()
     new_district = copy.deepcopy(district)
-    new_list = []
 
-    for battery in district.batteries:
+    # making list for batteries. Important when implementing constraints.
+    new_list = []
+    for battery in new_district.batteries:
         new_list.append(battery)
-    
-    #NIET LOOPEN
+
+
+    print(f"starting coordinates: {random_coordinates}")
+
     i = 0
     for i in range(4):
         new_list[i].x_coordinate = random_coordinates[i][0]
         new_list[i].y_coordinate = random_coordinates[i][1]
         i+=1
+
+    # Setting max parameters for start loop which searches for closest battery.
     grid = 50*50
     closest_difference = grid
+
+
+    previous_list = []
+
     while True:
         current_list = []
-        previous_list = []
         current_battery = None
-        for house in district.houses:
+        a = visualise(new_district)
+
+        for house in new_district.houses:
             selected_battery = None
             closest_difference = grid
 
             current_x = house.x_coordinate
             current_y = house.y_coordinate
-            for battery in district.batteries:
+            for battery in new_district.batteries:
                 
                 battery_x = battery.x_coordinate
                 battery_y = battery.y_coordinate
@@ -45,23 +57,27 @@ def k_means(district):
                     selected_battery = battery
             selected_battery.add_houses(house.id)
             selected_battery.add_houses_objects(house) 
-            #battery_dict[k].append((house.x_coordinate, house.y_coordinate))
+
 
         x_list = []
         y_list = []
 
-        for battery in district.batteries:
+        for battery in new_district.batteries:
             for house in battery.houses_objects:
-                i = 0
-                print(f"House number: {i} {house.x_coordinate}")
                 x_list.append(house.x_coordinate)
                 y_list.append(house.y_coordinate)
-                i+=1
+
+
             x_average = int(sum(x_list)/len(x_list))
             y_average = int(sum(y_list)/len(y_list))
             battery.x_coordinate = x_average
             battery.y_coordinate = y_average
             current_list.append((x_average, y_average))
+
+        print(f"previous list: {previous_list}")
+        print(f"corrected list: {current_list}")
+        a = visualise(new_district)
+
         if not previous_list:
             previous_list = current_list
         elif previous_list:
@@ -69,14 +85,11 @@ def k_means(district):
                 break
             else:
                 previous_list = current_list
-            
-                i = 0
-                for i in range(4):
-                    battery.x_coordinate = current_list[i][0]
-                    battery.y_coordinate = current_list[i][1]
-                    i+=1
 
 
-    for battery in district.batteries:
-        for house in battery.houses:
-            create_cable(battery.x_coordinate, battery.y_coordinate, house.x_coordinate, house.y_coordinate, district, house, battery)
+        print("One iteration finished.")
+        time.sleep(20)
+
+    for battery in new_district.batteries:
+        for house in battery.houses_objects:
+            create_cable(battery.x_coordinate, battery.y_coordinate, house.x_coordinate, house.y_coordinate, new_district, house, battery)
