b5940c1d5e9b2206ff7be9ba79adbc5f36b17db3
Neeltjepegu <NeeltjePegu>
2021-01-25 16:48:59 +0100

    added capacity constraint to the climber

--- code/functions/swap.py
+++ code/functions/swap.py
@@ -15,13 +15,14 @@ def swap(house, district):
     for battery in copy_district.batteries:
         # print(battery.current_capacity)
         copy_house.connected_battery = battery
+        
 
         closest_cable = find_closest_cable(battery, copy_house)
         copy_house.cables = []
         create_cable(copy_house.x_coordinate, copy_house.y_coordinate, closest_cable.x_coordinate, closest_cable.y_coordinate, copy_district, copy_house, battery)
         cable_cost = len(copy_house.cables * 9)
         print(f"battery: {battery.id}, cable cost: {cable_cost}")
-        if cable_cost < cheapest_connection:
+        if cable_cost < cheapest_connection and swap_capacity(battery, copy_house) == True:
             swapped = True
             cheapest_connection = cable_cost
 
@@ -30,5 +31,10 @@ def swap(house, district):
             create_cable(house.x_coordinate, house.y_coordinate, closest_cable.x_coordinate,
                          closest_cable.y_coordinate, district, house, battery)
 
-
     return swapped
+
+def swap_capacity(battery, copy_house):
+    if battery.capacity >= battery.current_capacity + copy_house.output:
+        return True
+    else:
+        return False
